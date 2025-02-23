ab3eeea98b7dbadf71d195437b16864983ad6edb
Ryu2312 <alessiasophia23@gmail.com>
2025-02-22 22:37:58 -0500

    actualizacion de la entidad de dominio

--- src/main/java/com/ryu/notes_api/domain/model/Note.java
+++ src/main/java/com/ryu/notes_api/domain/model/Note.java
@@ -1,19 +1,45 @@
 package com.ryu.notes_api.domain.model;
 
+import java.time.LocalDateTime;
+
 public class Note {
+    private Long id;
     private String title;
     private String body;
+    private LocalDateTime creationDate;
+    private boolean important;
+    private boolean pending;
 
-    public Note (String title, String body) {
+    public Note (Long id, String title, String body, LocalDateTime creationDate, boolean important, boolean pending) {
+        this.id = id;
         this.title = title;
         this.body = body;
+        this.creationDate = creationDate;
+        this.important = important;
+        this.pending = pending;
+    }
+
+    public Long getId () {
+        return id;
     }
 
     public String getTitle() {
         return title;
     }
-    
+
     public String getBody() {
         return body;
+    }
+
+    public LocalDateTime getCreationDate () {
+        return creationDate;
+    }
+
+    public boolean getImportant () {
+        return important;
+    }
+
+    public boolean getPending () {
+        return pending;
     }
 }
