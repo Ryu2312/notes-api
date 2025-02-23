ad43cd121ff638701d2fa7c05309b483e372d1c9
Ryu2312 <alessiasophia23@gmail.com>
2025-02-22 23:28:15 -0500

    definicion de interface de servicio

---
+++ src/main/java/com/ryu/notes_api/application/port/in/NotaUseCase.java
@@ -0,0 +1,14 @@
+package com.ryu.notes_api.application.port.in;
+
+import java.util.List;
+
+import com.ryu.notes_api.application.dto.NoteCreateDTO;
+import com.ryu.notes_api.application.dto.NoteUpdateDTO;
+import com.ryu.notes_api.domain.model.Note;
+
+public interface NotaUseCase {
+    Note newNote (NoteCreateDTO noteCreateDTO);
+    Note updateNote (NoteUpdateDTO noteUpdateDTO);
+    void deleteNote (Long id);
+    List <Note> listNote ();
+}
