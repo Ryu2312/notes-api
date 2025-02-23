5efb0761b3d2932d3c7d0530733c726d6efa26c1
Ryu2312 <alessiasophia23@gmail.com>
2025-02-23 01:07:20 -0500

    creacion de la logica de negocio

---
+++ src/main/java/com/ryu/notes_api/application/service/NoteService.java
@@ -0,0 +1,64 @@
+package com.ryu.notes_api.application.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.ryu.notes_api.application.dto.NoteCreateDTO;
+import com.ryu.notes_api.application.dto.NoteUpdateDTO;
+import com.ryu.notes_api.application.port.in.NoteUseCase;
+import com.ryu.notes_api.application.port.out.NoteRepository;
+import com.ryu.notes_api.domain.model.Note;
+
+public class NoteService implements NoteUseCase {
+
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @Override
+    public Note newNote(NoteCreateDTO noteCreateDTO) {
+       Note note = new Note();
+       note.setTitle(noteCreateDTO.getTitle());
+       note.setBody(noteCreateDTO.getBody());
+       note.setCreationDate(LocalDateTime.now());
+       note.setUpdateDate(LocalDateTime.now());
+       note.setPending(false);
+       note.setImportant(false);
+
+       Note savedNote = noteRepository.save(note);
+       return savedNote;
+    }
+
+    @Override
+    public Note updateNote(NoteUpdateDTO noteUpdateDTO) {
+        Note note = noteRepository.findById(noteUpdateDTO.getId())
+            .orElseThrow(() -> new IllegalArgumentException("Note not found"));
+        
+        if (noteUpdateDTO.getTitle() != null) {
+            note.setTitle(noteUpdateDTO.getTitle());
+        }
+
+        if (noteUpdateDTO.getBody() != null) {
+            note.setBody(noteUpdateDTO.getBody());
+        }
+
+        Note updateNote = noteRepository.save(note);
+        return updateNote;
+    }
+
+    @Override
+    public void deleteNote (Long id) {
+        Note note = noteRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Note not found"));
+
+        noteRepository.deleteById(note.getId());
+    }
+
+    @Override
+    public List<Note> listNote () {
+        return noteRepository.finAll();
+    }
+    
+}
--- src/main/java/com/ryu/notes_api/domain/model/Note.java
+++ src/main/java/com/ryu/notes_api/domain/model/Note.java
@@ -11,6 +11,8 @@ public class Note {
     private boolean important;
     private boolean pending;
 
+    public Note () {}
+
     public Note (Long id, String title, String body, LocalDateTime creationDate, boolean important, boolean pending) {
         this.id = id;
         this.title = title;
@@ -21,32 +23,39 @@ public class Note {
         this.pending = pending;
     }
 
-    public Long getId () {
-        return id;
+    public Long getId () { return id; }
+    public void setId (Long id) {
+        this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public String getTitle () { return title; }
+    public void setTitle (String title) {
+        this.title = title;
     }
 
-    public String getBody() {
-        return body;
+    public String getBody () { return body; }
+    public void setBody (String body) {
+        this.body = body;
     }
 
-    public LocalDateTime getCreationDate () {
-        return creationDate;
+    public LocalDateTime getCreationDate () { return creationDate; } 
+    public void setCreationDate (LocalDateTime creationDate) {
+        this.creationDate = creationDate;
     }
 
-    public LocalDateTime getUpdateDate () {
-        return updateDate;
-    } 
+    public LocalDateTime getUpdateDate () { return updateDate; }
+    public void setUpdateDate (LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
     
-    public boolean getImportant () {
-        return important;
+    public boolean getImportant () { return important; }
+    public void setImportant (boolean important) {
+        this.important = important;
     }
 
-    public boolean getPending () {
-        return pending;
+    public boolean getPending () { return pending; }
+    public void setPending (boolean pending) {
+        this.pending = pending;
     }
 
 }
